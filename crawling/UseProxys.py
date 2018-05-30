c4e0b4991d34b2d3588fb20a5fe2f6d9090c7744
wtt1002 <wtt1002@qq.com>
2018-05-30 21:53:09 +0800

    尝试新ip

--- crawling/UseProxys.py
+++ crawling/UseProxys.py
@@ -3,35 +3,41 @@
 
 from urllib import request,error
 import random
-#if __name__ == '__main__':
+
+
+# if __name__ == '__main__':
+
 
 proxy_list = [
-    {'http':'203.174.112.13:3128'},
-    {'http':'180.121.130.160:808'},
-    {'http':'125.64.17.100:808'},
-    {'http':'221.228.17.172:8181'},
-    {'http':'203.174.112.13:3128'}
+    {'http': '203.174.112.13:3128'},
+    {'http': '171.39.74.42:8123'},
+    {'http': '123.55.93.181:30419'},
+    {'http': '221.228.17.172:8181'},
+    {'http': '218.73.135.170:22617'}
 ]
 
 user_agent_list = [
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    # 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
     'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
     'Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
     'Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+',
     'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
     'MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
-    'UCWEB7.0.2.37/28/999',
+    # 'UCWEB7.0.2.37/28/999',
     'Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 Safari/534.6 TouchPad/1.0'
 ]
+
+
 def get_Agent():
     # 随机选择一个头部
     user_agent = random.choice(user_agent_list)
     print(user_agent)
     return user_agent
 
+
 def get_Proxy():
-    #随机选择一个代理
+    # 随机选择一个代理
     proxy = random.choice(proxy_list)
     print(proxy)
     return proxy
