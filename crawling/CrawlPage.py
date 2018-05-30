47e191fcd6a5283a2f0eed3d1699fff7b69e1b23
wtt1002 <wtt1002@qq.com>
2018-05-30 21:50:29 +0800

    出现异常，将异常页面记录下来

--- crawling/CrawlPage.py
+++ crawling/CrawlPage.py
@@ -3,10 +3,12 @@
 
 from urllib import request,error
 from crawling import UseProxys
+
+
 def getPage(url):
     # 访问网址
-    #url = 'http://www.baobeihuijia.com/list.aspx?tid=1'
-    #获取proxy 和 agent
+    # url = 'http://www.baobeihuijia.com/list.aspx?tid=1'
+    # 获取proxy 和 agent
     proxy = UseProxys.get_Proxy()
     user_agent = UseProxys.get_Agent()
     # 使用选择的代理构建代理处理器对象
@@ -21,13 +23,17 @@ def getPage(url):
     try:
         # 使用自己安装好的Opener
         response = request.urlopen(url)
+        # 读取相应信息并解码
+        html = response.read().decode("utf-8")
     except error.URLError as e:
-        print(e.reason)
+        print("获取网页失败"+e.reason)
+        with open('test1.txt', 'a', encoding='utf-8') as f:
+            f.write(url + '\n')
+            f.close()
+        return "fail"
 
-    # 读取相应信息并解码
-    html = response.read().decode("utf-8")
     # 打印信息
     print(html)
     # 关闭response
     response.close()
-    return html
+    return html;
