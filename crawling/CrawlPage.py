74e46a8ab7158af68ab920e235755f69df9dbfe7
wtt1002 <wtt1002@qq.com>
2018-05-31 22:03:40 +0800

    增加异常处理，去掉了代理IP

--- crawling/CrawlPage.py
+++ crawling/CrawlPage.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
 # by Wtting
 
-from urllib import request,error
+from urllib import error
 from crawling import UseProxys
-
+import requests
+from openpyxl.utils.exceptions import IllegalCharacterError
+from requests.exceptions import RequestException, ReadTimeout
 
 def getPage(url):
+
+    """
     # 访问网址
     # url = 'http://www.baobeihuijia.com/list.aspx?tid=1'
     # 获取proxy 和 agent
@@ -19,21 +23,35 @@ def getPage(url):
     opener.addheaders = [('User-Agent', user_agent)]
     # 安装Opener
     request.install_opener(opener)
-
+    """
+    # 获取随机的浏览器代理
+    user_agent = UseProxys.get_Agent()
+    # print('浏览器代理: ' + user_agent)
+    headers = {
+        "User-Agent": user_agent
+    }
     try:
         # 使用自己安装好的Opener
-        response = request.urlopen(url)
+        # response = request.urlopen(url)
+        response = requests.get(url, headers=headers)
+        # if response.status_code == 200:
+        #   return response.content.decode('utf-8')
         # 读取相应信息并解码
-        html = response.read().decode("utf-8")
+        # html = response.read().decode("utf-8")
+    except ReadTimeout:
+        print('Timeout')
+        return None
+    except ConnectionError:
+        print('Connection error')
+        return None
+    except RequestException:
+        print('Error')
+        return None
     except error.URLError as e:
-        print("获取网页失败"+e.reason)
-        with open('test1.txt', 'a', encoding='utf-8') as f:
-            f.write(url + '\n')
-            f.close()
-        return "fail"
+        return None
 
     # 打印信息
-    print(html)
+    # print(html)
     # 关闭response
     response.close()
-    return html;
+    return response.content.decode('utf-8')
