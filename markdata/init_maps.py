f35da05f52922090a633c11e5818f3d5104d5827
wtt1002 <wtt1002@qq.com>
2018-07-08 10:10:43 +0800

    省份与地区编码

--- markdata/init_maps.py
+++ markdata/init_maps.py
@@ -8,6 +8,7 @@ import xlrd
 
 """
     初始化map
+    districtCode.xlsx : 省份与城市编码表
 """
 # 地区与编码对应表
 district_maps = {}
@@ -15,22 +16,42 @@ district_maps = {}
 province_maps = {}
 
 
-def init_map():
+# 初始化省份map
+def init_map_pro():
     # 读取excel
     src_book = xlrd.open_workbook(r'E:\districtCode.xlsx')
     ws_src = src_book.sheet_by_name('Sheet')
     for i in range(0, ws_src.nrows):
         # 获取原excel中的一行数据
         row_values = ws_src.row_values(i)
-        # 获取地址
-        district_name = row_values[1]
-        # 获取编码
-        district_code = row_values[0]
+        # 获取省份地址名称
+        district_name = row_values[3]
+        # 获取省份编码
+        district_code = row_values[2]
         # print(district_name)
         # print(district_code)
         province_maps.update({district_name: district_code})
     print(province_maps)
 
 
+# 初始化地区map
+def init_map_dis():
+    # 读取excel
+    src_book = xlrd.open_workbook(r'E:\districtCode.xlsx')
+    ws_src = src_book.sheet_by_name('Sheet')
+    for i in range(0, ws_src.nrows):
+        # 获取原excel中的一行数据
+        row_values = ws_src.row_values(i)
+        # 获取地区城市名称
+        district_name = row_values[1]
+        # 获取地区城市编码
+        district_code = row_values[0]
+        # print(district_name)
+        # print(district_code)
+        district_maps.update({district_name: district_code})
+    print(district_maps)
+
+
 if __name__ == '__main__':
-    init_map()
+    init_map_pro()
+    init_map_dis()
